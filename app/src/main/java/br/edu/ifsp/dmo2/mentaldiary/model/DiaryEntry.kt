0afb71bc096064102cbcd26d648015bc36640e5f
Corujaoprogramador <deivid9pereira@gmail.com>
2025-07-12 12:14:02 -0300

    conexão com firebase e definindo model

--- app/src/main/java/br/edu/ifsp/dmo2/mentaldiary/model/DiaryEntry.kt
+++ app/src/main/java/br/edu/ifsp/dmo2/mentaldiary/model/DiaryEntry.kt
@@ -1,4 +1,11 @@
 package br.edu.ifsp.dmo2.mentaldiary.model
 
-class DiaryEntry {
-}
+data class DiaryEntry(
+    val id: String = "",
+    val userId: String = "",
+    val texto: String = "",
+    val humor: String = "",
+    val dataCriacao: com.google.firebase.Timestamp? = null,
+    val imagemUrl: String? = null,
+    val foiPorVoz: Boolean = false
+)
--- app/src/main/java/br/edu/ifsp/dmo2/mentaldiary/model/User.kt
+++ app/src/main/java/br/edu/ifsp/dmo2/mentaldiary/model/User.kt
@@ -1,4 +1,8 @@
 package br.edu.ifsp.dmo2.mentaldiary.model
 
-class User {
-}
+data class User(
+    val uid: String = "",
+    val nome: String = "",
+    val email: String = "",
+    val dataCriacao: com.google.firebase.Timestamp? = null
+)
